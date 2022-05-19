b027441062c99195946e10eca265f404b196ecab
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2022-05-19 13:24:10 +0530

    cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,24 @@ import { MdShoppingBasket} from "react-icons/md";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 import { app } from "../firebase.config";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
+  const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
+
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth,provider);
-    console.log(response);
+    const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
   };
   return (
     
@@ -59,7 +69,7 @@ const Header = () => {
         <div className="relative">
         <motion.img
             whileTap={{ scale: 0.6 }}
-            src={Avatar}
+            src={user ? user.photoURL : Avatar}
             className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
             alt="userprofile"
             onClick={login}
